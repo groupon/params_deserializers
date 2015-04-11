d3701817531436394bfa8875b357223238c3711a
Jesse Pinho <jesse@jessepinho.com>
2015-04-11 09:04:29 -0500

    Write specs for deserialize_params_with

--- spec/deserialize_params_with_spec.rb
+++ spec/deserialize_params_with_spec.rb
@@ -1,11 +1,33 @@
 require 'spec_helper'
 
 class StubController < ActionController::Base
-  deserialize_params_with Class.new(ParamsDeserializer) do
+  deserializer = Class.new(ParamsDeserializer) do
     attributes :foo
+  end
+
+  deserialize_params_with deserializer, only: :update
+
+  def update
+    render text: ''
   end
 end
 
-describe StubController do
-  it "has a deserialize method on the params object"
+describe StubController, type: :controller do
+  before :each do
+    Rails.application.routes.draw { get '/stub_controller' => 'stub#update' }
+  end
+
+  after :each do
+    Rails.application.reload_routes!
+  end
+
+  it 'can call the deserialized method on the params object' do
+    put :update
+    expect(controller.params).to respond_to :deserialized
+  end
+
+  it 'gets deserialized params when calling deserialized' do
+    put :update, foo: 'bar', baz: 'quux'
+    expect(controller.params.deserialized).to eql(foo: 'bar')
+  end
 end
