7a47bd23bd34b9598cbc8140445031f3ad22fd8a
Jesse Pinho <jesse@jessepinho.com>
2015-05-06 19:15:12 -0500

    Allow users to choose the name of the deserialized_params method

--- lib/deserialize_params_with.rb
+++ lib/deserialize_params_with.rb
@@ -1,14 +1,13 @@
 module ParamsDeserializers
   extend ActiveSupport::Concern
 
-  included do
-    attr_reader :deserialized_params
-  end
-
   module ClassMethods
     def deserialize_params_with(deserializer, options = {})
+      deserialized_params_name = options.delete(:as).try(:to_sym) || :deserialized_params
+      attr_reader deserialized_params_name
+
       before_filter(options) do
-        @deserialized_params = deserializer.new(params).deserialize
+        instance_variable_set("@#{deserialized_params_name}", deserializer.new(params).deserialize)
       end
     end
   end
--- spec/deserialize_params_with_spec.rb
+++ spec/deserialize_params_with_spec.rb
@@ -1,33 +1,54 @@
 require 'spec_helper'
 
-class StubController < ActionController::Base
-  include ParamsDeserializers
+describe 'deserialize_params_with', type: :controller do
+  context 'without an explicit method name' do
+    controller do
+      include ParamsDeserializers
 
-  deserialize_params_with(Class.new(ParamsDeserializer) do
-    attributes :foo
-  end, only: :update)
+      deserialize_params_with(Class.new(ParamsDeserializer) do
+        attributes :foo
+      end, only: :update)
 
-  def update
-    render text: ''
+      def update
+        render text: ''
+      end
+    end
+
+    it 'can call the deserialized_params getter' do
+      routes.draw { get 'update' => 'anonymous#update' }
+      put :update
+
+      expect(controller).to respond_to :deserialized_params
+    end
+
+    it 'gets deserialized params when calling the deserialized_params getter' do
+      routes.draw { get 'update' => 'anonymous#update' }
+      put :update, foo: 'bar', baz: 'quux'
+
+      expected = { foo: 'bar' }.with_indifferent_access
+      expect(controller.deserialized_params).to eql expected
+    end
   end
-end
 
-describe StubController, type: :controller do
-  around :each do |example|
-    Rails.application.routes.draw { get '/stub_controller' => 'stub#update' }
-    example.run
-    Rails.application.reload_routes!
-  end
+  context 'with an explicit method name' do
+    controller do
+      include ParamsDeserializers
 
-  it 'can call the deserialized_params getter' do
-    put :update
-    expect(controller).to respond_to :deserialized_params
-  end
+      deserialize_params_with(Class.new(ParamsDeserializer) do
+        attributes :foo
+      end, as: :deserialized_params_foo, only: :update)
 
-  it 'gets deserialized params when calling the deserialized_params getter' do
-    put :update, foo: 'bar', baz: 'quux'
+      def update
+        render text: ''
+      end
+    end
 
-    expected = { foo: 'bar' }.with_indifferent_access
-    expect(controller.deserialized_params).to eql expected
+    it 'can call the deserialized_params getter' do
+      routes.draw { get 'update' => 'anonymous#update' }
+      put :update
+
+      expect(controller).not_to respond_to :deserialized_params
+      expect(controller).to respond_to :deserialized_params_foo
+    end
   end
 end
